9d5b5a5e6b1495aa908d6056e45a5e2616ab5d7f
atsushi7534 <atsushi1007@outlook.jp>
2020-12-23 15:06:57 +0000

    [Final]bokkers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,61 @@
 class BooksController < ApplicationController
+  def top
+  end
+
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
   end
 
-  def edit
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id) , notice:'successfully'
+    else
+      render :index
+    end
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book) , notice:'successfully'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def back
+    book = Book.find(params[:id])
+    book.back
+    redirect_to book_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def books_params
+    params.require(:books).permit(:title, :body)
+  end
+
 end
